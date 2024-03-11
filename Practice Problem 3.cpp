501c434b80f1bb79b968867db64781ff66c8d03c
Chaitanya <20331a1281@mvgrce.edu.in>
2024-03-11 18:29:10 +0530

    3pp

---
+++ Practice Problem 3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int p,tp;
+    float roi;
+    cin>>p;
+    cin>>roi;
+    cin>>tp;
+    int si = (p*tp*roi)/100;
+    cout<<si;
+
+    return 0;
+}
